434a1bb2b5ce057645aceadc1766954a4be7bbae
tjacek <tjacek@student.agh.edu.pl>
2018-06-13 12:54:58 +0000

    find landmarks

--- markov.py
+++ markov.py
@@ -6,11 +6,17 @@ class FiniteMarkovChain(object):
         self.distributions=distributions
         self.state=None
 
-    def __call__(self,theta):
-        self.state=np.random.randint(len(self.distributions))
+    def __call__(self,theta,start=None):
+        if(start is None):
+            self.state=np.random.randint(len(self.distributions))
+        else:
+            self.state=start
         for i in range(theta):
             self.state=self.distributions[self.state]()
         return self.state
+    
+    def get_states(self):
+        return range(len(self.distributions))
 
 class FiniteDistribution(object):
     def __init__(self,states,probs):
@@ -18,7 +24,6 @@ class FiniteDistribution(object):
         self.probs=probs
 
     def __call__(self):
-        print(sum(self.probs))
         return np.random.choice(self.states, 1, p=self.probs)	
 		
 def make_markov_chain(nn_graph):
@@ -32,7 +37,19 @@ def make_markov_chain(nn_graph):
             for i in range(len(nn_graph))]
     return FiniteMarkovChain(dists)
 
-
+def find_landmarks(markov_chain,beta=100,theta=50,beta_theshold=1.30):
+    states=markov_chain.get_states()
+    hist=np.zeros((len(states),))
+    for state_i in states:
+        print(state_i)
+        for j in range(beta):
+            end_state=markov_chain(theta,state_i)
+            hist[end_state]+=1
+    treshold=beta_theshold*beta
+    landmarks=[ i 
+                for i,hist_i in enumerate(hist)
+                    if(hist_i>treshold)]
+    return landmarks
 
 if __name__ == "__main__": 
     make_markov_chain("mnist_graph")
